06a05dfe4b6178c61f0fda41d7d055faace39515
Charles Guertin <charlesguertin@live.ca>
2018-03-28 23:05:23 -0400

    Style note list

--- imports/ui/NoteListEmptyItem.jsx
+++ imports/ui/NoteListEmptyItem.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 export default NoteListEmptyItem = (props) => {
     return (
-        <div>
-            <h5>There are no notes.</h5>
+        <div className="empty-item">
             <p>Create a new note to get started!</p>
         </div>
     );
--- imports/ui/NoteListItem.jsx
+++ imports/ui/NoteListItem.jsx
@@ -6,13 +6,13 @@ import { withTracker } from 'meteor/react-meteor-data'
 
 
 export const NoteListItem = (props) => {
+    const className = props.note.selected ? 'item item--selected' : 'item'
     return (
-        <div onClick={() => {
+        <div className={className} onClick={() => {
             props.Session.set('selectedNoteId',props.note._id)}
         }>
-            <h5>{props.note.title || 'Untitled note'}</h5>
-            {props.note.selected ? 'Selected' : undefined }
-            <p>{moment(props.note.updatedAt).format('Y-MM-DD')}</p>
+            <h5 className="item__title">{props.note.title || 'Untitled note'}</h5>
+            <p className="item__subtitle">{moment(props.note.updatedAt).format('Y-MM-DD')}</p>
         </div>
     );
 };
